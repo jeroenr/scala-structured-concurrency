c94389464205371a543d1f78db96aacbb8aa9e5b
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2024-01-09 09:48:59 +0100

    Cleanup

--- src/test/scala/StructuredConcurrencyTest.scala
+++ src/test/scala/StructuredConcurrencyTest.scala
@@ -2,7 +2,9 @@ import Currency.USD
 import ox.*
 
 import scala.concurrent.duration.*
-import scala.util.*
+import munit.*
+
+import scala.concurrent.duration.Duration.fromNanos
 
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
@@ -10,36 +12,45 @@ enum Currency:
   case USD, EUR
 
 class ExchangeRateService(val name: String, val latency: Long, val rates: Map[Currency, Double]) {
-  def getExchangeRate(currency: Currency): Double = {
+  def getExchangeRate(currency: Currency): Double =
     println(s"${Thread.currentThread()} Fetching from $name...")
     Thread.sleep(latency)
     val result = rates.getOrElse(currency, 0.0)
     println(s"${Thread.currentThread()} Successfully fetched from $name!")
     result
-  }
 }
 
 class StructuredConcurrencyTest extends munit.FunSuite {
 
   private val exchanges = Seq(
-    ExchangeRateService("exchange 1", 1000, Map(USD -> 125.12)),
-    ExchangeRateService("exchange 2 (fastest)", 500, Map(USD -> 126.2)),
-    ExchangeRateService("exchange 3 (slowest)", 1500, Map(USD -> 124.12)),
+    ExchangeRateService(name = "EXCH-1", latency = 1000, rates = Map(USD -> 125.12)),
+    ExchangeRateService(name = "EXCH-2 (fastest)", latency = 500, rates = Map(USD -> 126.2)),
+    ExchangeRateService(name = "EXCH-3 (slowest)", latency = 1500, rates = Map(USD -> 124.12)),
   )
 
+  test("should get results concurrently") {
+   val (elapsedTime, _) = measureTimeMillis {
+      supervised {
+        exchanges.map { exch =>
+          fork {
+            exch.getExchangeRate(USD)
+          }
+        }
+      }
+    }
+    assert(elapsedTime < (exchanges.last.latency.millis + 300.millis), "Too slow!")
+  }
+
   test("should cancel slow exchanges") {
-    println()
-    println("Cancel slow exchanges test")
-    println()
     val results = supervised {
-      exchanges.map { ex =>
+      exchanges.map { exch =>
         fork {
           val currentThread = Thread.currentThread()
-          val res = Try(timeout(800.millis)(ex.getExchangeRate(USD))) match
-            case Success(res) =>
+          val res = timeoutOption(1100.millis)(exch.getExchangeRate(USD)) match
+            case Some(res) =>
               println(s"$currentThread Task completed with result '$res")
               Some(res)
-            case Failure(_) =>
+            case None =>
               println(s"$currentThread Task cancelled!")
               None
           res
@@ -49,22 +60,23 @@ class StructuredConcurrencyTest extends munit.FunSuite {
 
     results.foreach(res => println(s"Got rate: $res"))
 
-    assertEquals(results, Seq(126.2))
+    assertEquals(results, Seq(125.12, 126.2))
   }
 
   test("should get the result of the fastest exchange") {
-    println()
-    println("Race test")
-    println()
-    val winner = raceSuccess(exchanges.map { ex =>
-      () => {
-        val currentThread = Thread.currentThread()
-        val res = ex.getExchangeRate(USD)
-        println(s"$currentThread Task completed with result '$res")
-        res
-      }
-    })
+    val (elapsedTime, winner) = measureTimeMillis {
+      raceSuccess(exchanges.map { exch =>
+        () => exch.getExchangeRate(USD)
+      })
+    }
     println(s"Got rate: $winner")
     assertEquals(winner, 126.2)
+    assert(elapsedTime < (exchanges(1).latency.millis + 100.millis), "Too slow!")
   }
 }
+
+def measureTimeMillis[R](block: => R): (FiniteDuration, R) =
+  val startTime = System.nanoTime()
+  val result = block
+  val endTime = System.nanoTime()
+  (fromNanos(endTime - startTime), result)
